a38dcd50f3505e041124b7731204b1c7f68c1c5b
MohammadKhamalan <mohammad.khamalan@gmail.com>
2025-06-29 10:48:41 +0300

    edit command for create user

--- TravelAndAccommodationBookingPlatform.Application/Commands/UserCommands/CreateUserCommand.cs
+++ TravelAndAccommodationBookingPlatform.Application/Commands/UserCommands/CreateUserCommand.cs
@@ -10,12 +10,7 @@ namespace TravelAndAccommodationBookingPlatform.Application.Commands.UserCommand
 
 public class CreateUserCommand : IRequest<Guid>
 {
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public DateTime DateOfBirth { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Password { get; set; }
-    public int Role { get; set; } 
+    public CreateUserDto Dto { get; set; }
+
 }
 
