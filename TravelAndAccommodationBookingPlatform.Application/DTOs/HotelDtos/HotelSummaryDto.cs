05f9217efaf5d84bdcdecf1ec0e9f4eb8e3905ef
MohammadKhamalan <mohammad.khamalan@gmail.com>
2025-06-29 11:48:32 +0300

    Add hotel summary for included hotels in cities

---
+++ TravelAndAccommodationBookingPlatform.Application/DTOs/HotelDtos/HotelSummaryDto.cs
@@ -0,0 +1,8 @@
+﻿
+namespace TravelAndAccommodationBookingPlatform.Application.DTOs.HotelDtos;
+public class HotelSummaryDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public float Rating { get; set; }
+}
