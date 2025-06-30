8625aee83a246f108b3c0583b379ccb596a655c2
MohammadKhamalan <mohammad.khamalan@gmail.com>
2025-06-30 19:45:13 +0300

    create commands for image

---
+++ TravelAndAccommodationBookingPlatform.Application/Commands/ImageCommands/CreateImageCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Commands.ImageCommands;
+
+public class CreateImageCommand : IRequest<Guid>
+{
+    public string Url { get; set; }
+    public string Type { get; set; }
+    public string Format { get; set; }
+    public Guid? HotelId { get; set; }
+    public Guid? CityId { get; set; }
+    public string OwnerType { get; set; }
+}
---
+++ TravelAndAccommodationBookingPlatform.Application/Commands/ImageCommands/DeleteImageCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+
+namespace TravelAndAccommodationBookingPlatform.Application.Commands.ImageCommands;
+
+public class DeleteImageCommand : IRequest<bool>
+{
+    public DeleteImageCommand(Guid id)
+    {
+        Id = id;
+    }
+
+    public Guid Id { get; set; }
+}
