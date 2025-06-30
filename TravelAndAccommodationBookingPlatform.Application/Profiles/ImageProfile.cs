f40c87f7995a1538533f7e10815af180eb8afcb4
MohammadKhamalan <mohammad.khamalan@gmail.com>
2025-06-30 19:52:19 +0300

    Add mapping for image commands

--- TravelAndAccommodationBookingPlatform.Application/Profiles/ImageProfile.cs
+++ TravelAndAccommodationBookingPlatform.Application/Profiles/ImageProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using TravelAndAccommodationBookingPlatform.Application.Commands.ImageCommands;
 using TravelAndAccommodationBookingPlatform.Application.DTOs.ImageDtos;
 using TravelAndAccommodationBookingPlatform.Core.Entities;
 using TravelAndAccommodationBookingPlatform.Core.Enums;
@@ -14,8 +15,12 @@ public class ImageProfile : Profile
             .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Type.ToString()))
             .ForMember(dest => dest.Format, opt => opt.MapFrom(src => src.Format.ToString()))
             .ForMember(dest => dest.OwnerType, opt => opt.MapFrom(src => src.OwnerType.ToString()));
+        CreateMap<CreateImageCommand, Image>()
+    .ForMember(dest => dest.Type, opt => opt.MapFrom(src => Enum.Parse<ImageType>(src.Type, true)))
+    .ForMember(dest => dest.Format, opt => opt.MapFrom(src => Enum.Parse<ImageFormat>(src.Format, true)))
+    .ForMember(dest => dest.OwnerType, opt => opt.MapFrom(src => Enum.Parse<ImageOwnerType>(src.OwnerType, true)));
 
-        
+
         CreateMap<CreateImageRequestDto, Image>()
             .ForMember(dest => dest.Type, opt => opt.MapFrom(src => Enum.Parse<ImageType>(src.Type, true)))
             .ForMember(dest => dest.Format, opt => opt.MapFrom(src => Enum.Parse<ImageFormat>(src.Format, true)))
