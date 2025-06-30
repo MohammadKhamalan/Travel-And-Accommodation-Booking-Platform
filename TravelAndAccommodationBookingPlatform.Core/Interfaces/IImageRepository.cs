70526ab4754a9709daa2bc0f84c69e2ba2cdcea6
MohammadKhamalan <mohammad.khamalan@gmail.com>
2025-06-30 19:49:58 +0300

    Add interface for image functions

---
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/IImageRepository.cs
@@ -0,0 +1,13 @@
+﻿using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces;
+
+public interface IImageRepository
+{
+    Task<Image?> GetByIdAsync(Guid id);
+    Task<List<Image>> GetImagesByHotelIdAsync(Guid hotelId);
+    Task<List<Image>> GetImagesByCityIdAsync(Guid cityId);
+    Task<Image> InsertAsync(Image image);
+    Task DeleteAsync(Guid id);
+    Task SaveChangesAsync();
+}
