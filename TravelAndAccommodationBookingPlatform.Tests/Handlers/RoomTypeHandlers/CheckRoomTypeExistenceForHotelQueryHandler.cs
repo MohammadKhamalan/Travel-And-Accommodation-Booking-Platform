e335bb16122c76e7b0f54946fd01e1ff995c01a1
MohammadKhamalan <mohammad.khamalan@gmail.com>
2025-07-02 19:07:53 +0300

    Add unit tests for RoomType handlers

---
+++ TravelAndAccommodationBookingPlatform.Tests/Handlers/RoomTypeHandlers/CheckRoomTypeExistenceForHotelQueryHandler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using TravelAndAccommodationBookingPlatform.Application.Handlers.RoomTypeHandlers;
+using TravelAndAccommodationBookingPlatform.Application.Queries.RoomTypeQueries;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces;
+using Xunit;
+
+namespace TravelAndAccommodationBookingPlatform.Tests.Handlers.RoomTypeHandlers;
+
+public class CheckRoomTypeExistenceForHotelQueryHandlerTests
+{
+    [Fact]
+    public async Task Handle_ShouldReturnTrue_WhenRoomTypeExists()
+    {
+        // Arrange
+        var hotelId = Guid.NewGuid();
+        var roomTypeId = Guid.NewGuid();
+
+        var repoMock = new Mock<IRoomTypeRepository>();
+        repoMock.Setup(r => r.CheckRoomTypeExistenceForHotel(hotelId, roomTypeId))
+                .ReturnsAsync(true);
+
+        var handler = new CheckRoomTypeExistenceForHotelQueryHandler(repoMock.Object);
+        var query = new CheckRoomTypeExistenceForHotelQuery
+        {
+            HotelId = hotelId,
+            RoomTypeId = roomTypeId
+        };
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnFalse_WhenRoomTypeDoesNotExist()
+    {
+        // Arrange
+        var hotelId = Guid.NewGuid();
+        var roomTypeId = Guid.NewGuid();
+
+        var repoMock = new Mock<IRoomTypeRepository>();
+        repoMock.Setup(r => r.CheckRoomTypeExistenceForHotel(hotelId, roomTypeId))
+                .ReturnsAsync(false);
+
+        var handler = new CheckRoomTypeExistenceForHotelQueryHandler(repoMock.Object);
+        var query = new CheckRoomTypeExistenceForHotelQuery
+        {
+            HotelId = hotelId,
+            RoomTypeId = roomTypeId
+        };
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+}
---
+++ TravelAndAccommodationBookingPlatform.Tests/Handlers/RoomTypeHandlers/CreateRoomTypeCommandHandler.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TravelAndAccommodationBookingPlatform.Application.Commands.RoomTypeCommands;
+using TravelAndAccommodationBookingPlatform.Application.DTOs.RoomTypeDtos;
+using TravelAndAccommodationBookingPlatform.Application.Handlers.RoomTypeHandlers;
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+using TravelAndAccommodationBookingPlatform.Core.Enums;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces;
+using Xunit;
+
+namespace TravelAndAccommodationBookingPlatform.Tests.Handlers.RoomTypeHandlers;
+
+public class CreateRoomTypeCommandHandlerTests
+{
+    [Fact]
+    public async Task Handle_ShouldCreateRoomTypeAndReturnId()
+    {
+        // Arrange
+        var dto = new CreateRoomTypeDto
+        {
+            RoomCategory = "Double",
+            PricePerNight = 150,
+            AmenityIds = new() { Guid.NewGuid() }
+        };
+
+        var command = new CreateRoomTypeCommand { RoomType = dto };
+        var mappedRoomType = new RoomType
+        {
+            RoomTypeId = Guid.NewGuid(),
+            RoomCategory = RoomCategory.Double,
+            PricePerNight = dto.PricePerNight
+        };
+
+        var repoMock = new Mock<IRoomTypeRepository>();
+        var mapperMock = new Mock<IMapper>();
+
+        mapperMock.Setup(m => m.Map<RoomType>(dto)).Returns(mappedRoomType);
+        repoMock.Setup(r => r.InsertAsync(mappedRoomType)).ReturnsAsync(mappedRoomType);
+
+        repoMock.Setup(r => r.SaveChangesAsync()).Returns(Task.CompletedTask);
+
+        var handler = new CreateRoomTypeCommandHandler(repoMock.Object, mapperMock.Object);
+
+        // Act
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.Should().Be(mappedRoomType.RoomTypeId);
+    }
+}
