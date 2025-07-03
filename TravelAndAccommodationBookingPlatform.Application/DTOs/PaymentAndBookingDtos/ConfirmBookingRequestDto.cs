1e70c9a8a15ef03b85dcc0854cfe822160c43941
MohammadKhamalan <mohammad.khamalan@gmail.com>
2025-07-03 20:57:59 +0300

    Create Dto to link the payment with booking

---
+++ TravelAndAccommodationBookingPlatform.Application/DTOs/PaymentAndBookingDtos/ConfirmBookingRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAndAccommodationBookingPlatform.Application.DTOs.PaymentAndBookingDtos;
+public class ConfirmBookingRequestDto
+{
+    public Guid PendingBookingId { get; set; }
+    public decimal Amount { get; set; }
+    public string Currency { get; set; } = "usd";
+}
---
+++ TravelAndAccommodationBookingPlatform.Application/DTOs/PaymentAndBookingDtos/PaymentAndBookingRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAndAccommodationBookingPlatform.Core.Enums;
+
+namespace TravelAndAccommodationBookingPlatform.Application.DTOs.PaymentAndBookingDtos;
+public class PaymentAndBookingRequestDto
+{
+    public Guid RoomId { get; set; }
+    public Guid UserId { get; set; }
+    public DateTime CheckInDate { get; set; }
+    public DateTime CheckOutDate { get; set; }
+    public decimal Amount { get; set; }
+    public string Currency { get; set; } = "usd";
+    public PaymentMethod Method { get; set; } = PaymentMethod.CreditCard;
+}
