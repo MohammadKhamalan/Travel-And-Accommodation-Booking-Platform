f1823a8c2de619530af65b3fce5b031fec51af94
MohammadKhamalan <mohammad.khamalan@gmail.com>
2025-07-03 20:57:00 +0300

    Create Dto for pending booking creating

---
+++ TravelAndAccommodationBookingPlatform.Application/DTOs/BookingDtos/PendingBookingCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Application.DTOs.BookingDtos;
+
+public class PendingBookingCreateDto
+{
+    public Guid RoomId { get; set; }
+    public Guid UserId { get; set; }
+    public DateTime CheckInDate { get; set; }
+    public DateTime CheckOutDate { get; set; }
+}
