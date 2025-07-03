9e98c7d73c6efd1253976c7016777ef3b88c21b8
MohammadKhamalan <mohammad.khamalan@gmail.com>
2025-07-03 21:10:07 +0300

    implement all functions of interface

---
+++ TravelAndAccommodationBookingPlatform.Infrastructure/Persistence/Repository/StripePaymentService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Stripe;
+using TravelAndAccommodationBookingPlatform.Core.Interfaces;
+
+namespace TravelAndAccommodationBookingPlatform.Infrastructure.ExternalServices;
+
+public class StripePaymentService : IPaymentService
+{
+    public StripePaymentService(IConfiguration configuration)
+    {
+        StripeConfiguration.ApiKey = configuration["Stripe:SecretKey"];
+    }
+
+    public async Task<string> CreatePaymentIntentAsync(decimal amount, string currency)
+    {
+        var options = new PaymentIntentCreateOptions
+        {
+            Amount = (long)(amount * 100), 
+            Currency = currency,
+            PaymentMethodTypes = new List<string> { "card" },
+        };
+
+        var service = new PaymentIntentService();
+        var intent = await service.CreateAsync(options);
+        return intent.ClientSecret;
+    }
+}
