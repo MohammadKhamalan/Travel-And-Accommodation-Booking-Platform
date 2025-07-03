442b542cd864ee831ef0daa727dcd759553deb09
MohammadKhamalan <mohammad.khamalan@gmail.com>
2025-07-03 20:59:34 +0300

    Create Entity for pending Bookings table

---
+++ TravelAndAccommodationBookingPlatform.Core/Entities/PendingBooking.cs
@@ -0,0 +1,16 @@
+﻿
+namespace TravelAndAccommodationBookingPlatform.Core.Entities;
+public class PendingBooking
+{
+    public Guid Id { get; set; }
+    public Guid RoomId { get; set; }
+    public Guid UserId { get; set; }
+    public DateTime CheckInDate { get; set; }
+    public DateTime CheckOutDate { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public double Price { get; set; }
+
+    public Room Room { get; set; }
+    public User User { get; set; }
+}
+
