5634c87bd8d399a54791addbbe2df5f4d9c9d905
MohammadKhamalan <mohammad.khamalan@gmail.com>
2025-07-03 21:03:19 +0300

    Create Interface for paymentservice

---
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/IPaymentService.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces;
+
+public interface IPaymentService
+{
+    Task<string> CreatePaymentIntentAsync(decimal amount, string currency);
+}
