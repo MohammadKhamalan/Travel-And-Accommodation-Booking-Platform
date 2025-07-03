8bcdd4b9a1c1d58dec3fb6568aa7f6af4f08bbaa
MohammadKhamalan <mohammad.khamalan@gmail.com>
2025-07-03 21:05:34 +0300

    Create Interface for pending Booking

---
+++ TravelAndAccommodationBookingPlatform.Core/Interfaces/IPendingBookingRepository.cs
@@ -0,0 +1,13 @@
+﻿
+
+using TravelAndAccommodationBookingPlatform.Core.Entities;
+
+namespace TravelAndAccommodationBookingPlatform.Core.Interfaces;
+public interface IPendingBookingRepository
+{
+    Task<PendingBooking> InsertAsync(PendingBooking entity);
+    Task DeleteAsync(Guid id);
+    Task<PendingBooking?> GetPendingByIdAsync(Guid id);
+    Task<User?> GetByUserIdAsync(Guid id);
+    Task DeletePendingAsync(Guid id);
+}
