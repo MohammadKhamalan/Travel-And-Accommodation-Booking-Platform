a5781a91717d908496e6cc457a6624823578ba99
MohammadKhamalan <mohammad.khamalan@gmail.com>
2025-07-07 01:54:59 +0300

    Add a util for password hashing for the user

---
+++ TravelAndAccommodationBookingPlatform.Application/Utils/PasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCrypt.Net;
+using BCryptNet = BCrypt.Net.BCrypt;
+namespace TravelAndAccommodationBookingPlatform.Application.Utils
+{
+    public class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            return BCryptNet.EnhancedHashPassword(password, HashType.SHA384);
+        }
+
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            return BCryptNet.EnhancedVerify(password, hashedPassword, HashType.SHA384);
+        }
+    }
+}
